5b4f09280c2a506c55ee9ccae45ddaa49fb478b3
Shamiul-Lipu <shamiullipu@gmail.com>
2024-01-11 21:15:57 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-xl font-bold">Redux Todo</h1>
+      <Button>Shadcn</Button>
+    </>
+  );
+}
+
+export default App;
